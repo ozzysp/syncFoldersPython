3725d73ecca6e406dfa01641ffce3e7cbac0804f
Ozzy Scuba <ozzysp@icloud.com>
2023-04-12 13:32:57 -0300

    Create main.py first function

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 "****************************************************************************
 "*  This script was created by:                                             *
-"*  Ozzy -               *
+"*  Ozzy -  https://github.com/ozzysp/syncFoldersPython                     *
+"*  Date   :  2021-04-01                                                    *
+"*  Version:  1.0.0                                                         *
+"*  License:  MIT                                                           *
+"****************************************************************************
+
+if main() == "__main__":
+    print("This script was created by: Ozzy - https://github.com/ozzysp/syncFoldersPython")
+    print("Date   : 2021-04-01")
+    print("Version: 1.0.0")
+    print("License: MIT")
+    print("")
