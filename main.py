d00080467a40231eb3d37b1c5af95beb39bd92d6
Ozzy Scuba <ozzysp@icloud.com>
2023-04-13 23:08:32 -0300

    Initial commit

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-"****************************************************************************
-"*  This script was created by:                                             *
-"*  Ozzy -  https://github.com/ozzysp/syncFoldersPython                     *
-"*  Date   :  2021-04-01                                                    *
-"*  Version:  1.0.0                                                         *
-"*  License:  MIT                                                           *
-"****************************************************************************
+#****************************************************************************
+#*  This script was created by:                                             *
+#*  Ozzy -  https://github.com/ozzysp/syncFoldersPython                     *
+#*  Date   :  2021-04-01                                                    *
+#*  Version:  1.0.0                                                         *
+#*  License:  MIT                                                           *
+#****************************************************************************
 
-if main() == "__main__":
-    print("This script was created by: Ozzy - https://github.com/ozzysp/syncFoldersPython")
-    print("Date   : 2021-04-01")
-    print("Version: 1.0.0")
-    print("License: MIT")
-    print("")
+def get_paths():
+    source_dir = input("Enter the source folder path: ")
+    replica_dir = input("Enter the destin folder path: ")
+    return source_dir, replica_dir
+
+
+def get_interval():
+    interval = input("Enter the sync interval (in hours): ")
+    return int(interval)
+
+
+def get_log_path():
+    log_path = input("Enter the log file path: ")
+    return log_path
