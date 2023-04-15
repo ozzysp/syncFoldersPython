1f3faf50277ec8668b3cc723dabb4589ed2759fe
Ozzy Scuba <ozzysp@icloud.com>
2023-04-15 00:08:39 -0300

    Creates prompt for user in terminal

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-#****************************************************************************
-#*  This script was created by:                                             *
-#*  Ozzy -  https://github.com/ozzysp/syncFoldersPython                     *
-#*  Date   :  2021-04-01                                                    *
-#*  Version:  1.0.0                                                         *
-#*  License:  MIT                                                           *
-#****************************************************************************
-
-def get_paths():
-    source_dir = input("Enter the source folder path: ")
-    replica_dir = input("Enter the destin folder path: ")
-    return source_dir, replica_dir
+# ****************************************************************************
+# *  This script was created by:                                             *
+# *  Ozzy -  https://github.com/ozzysp/syncFoldersPython                     *
+# *  Date   :  2021-04-01                                                    *
+# *  Version:  1.0.0                                                         *
+# *  License:  GNU                                                           *
+# ****************************************************************************
 
 
-def get_interval():
-    interval = input("Enter the sync interval (in hours): ")
-    return int(interval)
+def get_user_input():
+    source_dir_path = input("Enter the path to the source directory: ")
+    replica_dir_path = input("Enter the path to the replica directory: ")
+    log_dir_path = input("Enter the path to the log directory: ")
+    interval = int(input("Enter the interval of time for synchronization routine (in minutes): "))
+    return source_dir_path, replica_dir_path, log_dir_path, interval
 
 
-def get_log_path():
-    log_path = input("Enter the log file path: ")
-    return log_path
+source_dir_path, replica_dir_path, log_dir_path, interval = get_user_input()
